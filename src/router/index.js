c841607e65bde5cb1f9aa812a2fd0928109fc6ba
VIDUSHISAINI29 <vidushisaini29@gmail.com>
2024-11-24 22:20:26 +0530

    book store start

---
+++ src/router/index.js
@@ -0,0 +1,28 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+const router = createRouter({
+   history: createWebHistory(import.meta.env.BASE_URL),
+   routes: [
+      {
+         path: "/",
+         component: () => import("@/layout/AppLayout.vue"),
+         children: [
+            {
+               path: "home",
+               name: "home",
+               component: () => import("@/pages/Home/Home.vue"),
+               children: [
+                  {
+                     path: ":uri",
+                     name: "homeContent",
+                     component: () => import("@/pages/Home/Content.vue"),
+                     props: true,
+                  },
+               ],
+            },
+         ],
+      },
+   ],
+});
+
+export default router;
